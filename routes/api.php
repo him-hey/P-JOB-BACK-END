86b677003c969f7f0b99c4747e01893363c1474e
him-hey <him.hey@passerellesnumeriques.org>
2022-10-20 18:57:51 +0700

    add login feature

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/auth/register', [AuthenticationController::class, 'register']);
+Route::post('/auth/login', [AuthenticationController::class, 'login']);
